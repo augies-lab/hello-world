d56f30afacf4de9f4edcd9075435bd331677b564
augies-lab <184285aug@gmail.com>
2019-09-07 11:53:17 -0700

    Test class created to verify git works.

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+/*This is an example class to check if
+my github account is properly configured. */
+
+public class HelloWorld {
+	public static void main(String args[])
+	{
+	System.out.println("Hello world!");
+	}
+}
